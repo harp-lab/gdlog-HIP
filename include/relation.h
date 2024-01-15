fba14959d3c2d17f0c966f00ce236d4fe3fbdd80
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2024-01-15 10:51:02 -0600

    Update relation.h

--- include/relation.h
+++ include/relation.h
@@ -7,7 +7,7 @@
 #define RADIX_SORT_THRESHOLD 0
 #endif
 #ifndef FULL_BUFFER_VEC_MULTIPLIER
-#define FULL_BUFFER_VEC_MULTIPLIER 3
+#define FULL_BUFFER_VEC_MULTIPLIER 2
 #endif
 
 enum RelationVersion { DELTA, FULL, NEWT };
